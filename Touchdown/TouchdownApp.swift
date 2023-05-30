4c98568d078aca3fa09c57d31ef8a402c655f942
ibnu naufal <ibnunaufal5758@gmail.com>
2023-05-30 14:51:27 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Macbook Pro on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
